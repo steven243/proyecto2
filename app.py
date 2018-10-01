03f75e2c99d3d453c4b8c9d9eb201e4fc059de67
steven243 <43686665+steven243@users.noreply.github.com>
2018-10-01 08:33:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
+    # Enlace a PUERTO si está definido, de lo contrario será 5000 por defecto.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
