36d7b6b507231f53aab4a9bc0c91fdb5890c227d
steven243 <43686665+steven243@users.noreply.github.com>
2018-10-04 06:45:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,4 @@ def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    # Enlace a PUERTO si está definido, de lo contrario será 5000 por defecto.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
